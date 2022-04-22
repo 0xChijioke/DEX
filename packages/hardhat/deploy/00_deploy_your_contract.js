c99d539c256fa2214dd58071e1cceef122a343b6
chijoke ogbuanya <chijokeogbuanya@gmail.com>
2022-04-22 19:49:21 +0100

    correct amount of ethers parsed

--- packages/hardhat/deploy/00_deploy_your_contract.js
+++ packages/hardhat/deploy/00_deploy_your_contract.js
@@ -49,8 +49,8 @@ module.exports = async ({ getNamedAccounts, deployments, getChainId }) => {
   // If you are going to the testnet make sure your deployer account has enough ETH
   await balloons.approve(dex.address, ethers.utils.parseEther("100"));
   console.log("INIT exchange...");
-  await dex.init(ethers.utils.parseEther("" + (3 * 10 ** 18)), {
-    value: ethers.utils.parseEther("3"),
+  await dex.init(ethers.utils.parseEther("5"), {
+    value: ethers.utils.parseEther("5"),
     gasLimit: 200000,
   });
 };
